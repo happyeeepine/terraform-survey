ab9e50ee94cfd1d877f91c4d7bf532ed9b244486
EC2 Default User <ec2-user@ip-172-30-1-51.ap-northeast-1.compute.internal>
2022-09-29 20:35:00 +0000

    構成変更 moduleディレクトリ名変更

--- ecs_service.tf
+++ ecs_service.tf
@@ -29,7 +29,7 @@ resource "aws_ecs_service" "example" {
 }
 
 module "nginx_sg" {
-  source      = "./module/security_group"
+  source      = "./modules/security_group"
   name        = "nginx-sg"
   vpc_id      = aws_vpc.example.id
   port        = 80
